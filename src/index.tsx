029fdb680090312ad26d43ee7ce2f31e9e27d88b
MelnykovSV <svmelnykov@gmail.com>
2023-05-20 21:22:31 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,11 @@ import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
